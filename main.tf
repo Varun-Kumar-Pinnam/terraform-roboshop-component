2ced6709c7e59f272040ab679d050862cb0c24fc
Varun <varunkumar.pinnam@gmail.com>
2026-03-28 10:25:45 +0530

    infra

--- main.tf
+++ main.tf
@@ -109,7 +109,7 @@ resource "aws_autoscaling_group" "main" {
   min_size                  = 1
   health_check_grace_period = 120
   health_check_type         = "ELB"
-  desired_capacity          = 2
+  desired_capacity          = 1
   force_delete              = false
   vpc_zone_identifier       = [local.private_subnet_id]
   target_group_arns         = [aws_lb_target_group.main.arn]
